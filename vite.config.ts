6980294d761d53c9586729ced85fa6606e625e02
Dmitry <inaumovd@gmail.com>
2025-09-07 16:22:09 +0700

    Remove unused bundled JavaScript asset from documentation directory.

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,7 @@ export default defineConfig(({ mode }) => ({
     },
     plugins: [
         react(),
-        mode === 'development' &&
-        componentTagger(),
+
     ].filter(Boolean),
     resolve: {
         alias: {
